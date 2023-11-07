b37a5d3cba625dd0a187458f40db80b9f48db17b
Mitchell Byrden <mitch@xpulsenetwork.com>
2023-11-08 03:07:35 +1100

    Added made config.base variable for prod/dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,34 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import dsv from '@rollup/plugin-dsv'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import dsv from '@rollup/plugin-dsv';
 
-import purgecss from '@fullhuman/postcss-purgecss'
+import purgecss from '@fullhuman/postcss-purgecss';
 
-const plugins = [svelte(), dsv()]
+const plugins = [svelte(), dsv()];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
+  // Base configuration that applies to both build and dev
+  const config = {
+    plugins,
+  };
+
   // Only run PurgeCSS in production builds
   if (command === 'build') {
-    return {
-      plugins,
-      css: {
-        postcss: {
-          plugins: [
-            purgecss({
-              content: ['./**/*.html', './**/*.svelte'],
-              safelist: ['pre', 'code']
-            })
-          ]
-        }
+    config.css = {
+      postcss: {
+        plugins: [
+          purgecss({
+            content: ['./**/*.html', './**/*.svelte'],
+            safelist: ['pre', 'code'],
+          }),
+        ],
       },
-      base: '/' //For custom domain
-    }
+    };
+    config.base = './'; // For custom domain when building for production
   } else {
-    return {
-      plugins
-    }
+    config.base = '/'; // Base set to / for development
   }
-})
+
+  return config;
+});
