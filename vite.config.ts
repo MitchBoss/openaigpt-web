3961d510a8af6d8b704832dbad1e6e0e2f581c3a
Mitchell Byrden <mitch@xpulsenetwork.com>
2023-11-08 19:35:51 +1100

    lint fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import dsv from '@rollup/plugin-dsv';
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import dsv from '@rollup/plugin-dsv'
 
-import purgecss from '@fullhuman/postcss-purgecss';
+import purgecss from '@fullhuman/postcss-purgecss'
 
-const plugins = [svelte(), dsv()];
+const plugins = [svelte(), dsv()]
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode, ssrBuild }) => {
   // Base configuration that applies to both build and dev
   const config = {
-    plugins,
-  };
+    plugins
+  }
 
   // Only run PurgeCSS in production builds
   if (command === 'build') {
@@ -20,15 +20,15 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
         plugins: [
           purgecss({
             content: ['./**/*.html', './**/*.svelte'],
-            safelist: ['pre', 'code'],
-          }),
-        ],
-      },
-    };
-    config.base = './'; // For custom domain when building for production
+            safelist: ['pre', 'code']
+          })
+        ]
+      }
+    }
+    config.base = './' // For custom domain when building for production
   } else {
-    config.base = '/'; // Base set to / for development
+    config.base = '/' // Base set to / for development
   }
 
-  return config;
-});
+  return config
+})
