d4922330a6b841d05ddeab15fe54e0c0e719b9fd
Mitchell Byrden <mitch@xpulsenetwork.com>
2023-11-08 14:35:05 +1100

    Update API to api.xpulsenetwork.com

--- src/lib/providers/openai/request.svelte
+++ src/lib/providers/openai/request.svelte
@@ -25,6 +25,8 @@ export const chatRequest = async (
         method: 'POST',
         headers: {
           Authorization: `Bearer ${getApiKey()}`,
+          'X-Api-Key: Bearer': 'e355aee7102495542aec6ea0f6580eac14308bad6932b604f90da5bf8cc08d74',
+          'X-User-Id': 'myuser@TextGPT.XPN.com',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(request),
@@ -147,6 +149,8 @@ export const imageRequest = async (
         method: 'POST',
         headers: {
           Authorization: `Bearer ${getApiKey()}`,
+          'X-Api-Key: Bearer': 'e355aee7102495542aec6ea0f6580eac14308bad6932b604f90da5bf8cc08d74',
+          'X-User-Id': 'myuser@TextGPT.XPN.com',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(request),
